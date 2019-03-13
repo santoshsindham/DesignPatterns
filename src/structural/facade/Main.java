9a28c4287aedf4732be350987f6375d397f5379a
santoshsindham <santoshkumar.sindham@tesco.com>
2019-03-13 23:49:24 +0530

    facade pattern

---
+++ src/structural/facade/Main.java
@@ -0,0 +1,13 @@
+package structural.facade;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        ReportGeneratorFacade reportGeneratorFacade = new ReportGeneratorFacade();
+
+        reportGeneratorFacade.generateReport(ReportType.HTML, null, null);
+
+        reportGeneratorFacade.generateReport(ReportType.PDF, null, null);
+    }
+}
---
+++ src/structural/facade/ReportType.java
@@ -0,0 +1,5 @@
+package structural.facade;
+
+public enum ReportType {
+    PDF, HTML
+}
