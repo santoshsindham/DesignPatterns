3f8214d6a6f6e3915044223c81133852bb298e0c
santoshsindham <santoshkumar.sindham@tesco.com>
2019-03-19 21:55:29 +0530

    state pattern

--- src/behavioral/state/Main.java
+++ src/behavioral/state/Main.java
@@ -1,5 +1,10 @@
 package behavioral.state;
 
+/*
+In state pattern allows an object to alter its behavior when its internal state changes.
+The object will appear to change its class. There shall be a separate concrete class per possible state of an object.
+
+ */
 public class Main
 {
     public static void main(String[] args)
